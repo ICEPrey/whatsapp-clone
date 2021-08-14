1660395932ebc8877fb468b97b5266a926d4968f
ICEPrey <abuwswork@gmail.com>
2021-08-14 08:45:33 -0700

    Fix

---
+++ components/Chat.tsx
@@ -0,0 +1,9 @@
+function Chat({ id, users }: any) {
+  return (
+    <div>
+      <p className="text-black">Recepient Email</p>
+    </div>
+  );
+}
+
+export default Chat;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,13 +8,14 @@ import * as EmailValidator from "email-validator";
 import { auth, db } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
+import Chat from "./Chat";
 
 function Sidebar() {
   const [user] = useAuthState(auth);
   const userChatRef = db
     .collection("chats")
     .where("users", "array-contains", user.email);
-  const [chatSnapshot] = useCollection(userChatRef);
+  const [chatsSnapshot] = useCollection(userChatRef);
   const createChat = (): string => {
     const input = prompt(
       "Please enter an email address for the user you wish to chat with"
@@ -24,7 +25,7 @@ function Sidebar() {
 
     if (
       EmailValidator.validate(input) &&
-      !chatAlreadyExists &&
+      !chatAlreadyExists(input) &&
       input !== user.email
     ) {
       db.collection("chats").add({
@@ -34,7 +35,7 @@ function Sidebar() {
   };
 
   const chatAlreadyExists = (recipientEmail: string) =>
-    !!chatSnapshot?.docs.find(
+    !!chatsSnapshot?.docs.find(
       (chat) =>
         chat.data().users.find((user: string) => user === recipientEmail)
           ?.length > 0
@@ -63,6 +64,9 @@ function Sidebar() {
       <Button className="w-full" onClick={createChat}>
         Start A New Chat
       </Button>
+      {chatsSnapshot?.docs.map((chat) => (
+        <Chat key={chat.id} id={chat.id} user={chat.data().users} />
+      ))}
     </div>
   );
 }
